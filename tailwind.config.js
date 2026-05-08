52700ff81591a15f575f0675ebc7ca15512c8418
ali_hashemian <aho.hashemian@gmail.com>
2026-05-09 01:06:43 +0330

    Setup project directory structure, core modules, and tailwind configuration

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./core/**/*.js", "./apps/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
